285220d194a470fbf9d06133fe9da1fea9ed2561
Carlos Mtnez <krlitosmtnez@gmail.com>
2024-06-10 22:23:38 +0200

    Ejercicio de TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+console.log("Tienes un grupo de 6 amigos y quieres invitarlos a cenar por tu cumpleaños.");
+console.log("Solo puedes permitirte invitar a las bebidas, ya que estás un poco ajustado de dinero.");
+console.log("Tienes un ticket de cena que cuesta 120 € y en el que ya se incluyen las bebidas por un valor de 18 €.");
+console.log("Calcula cuánto tendría que pagar cada comensal para dividir los costos de manera equitativa.");
+console.log("Utiliza JavaScript para hacer el cálculo y mostrar el resultado por consola.");
+console.log("SOLUCIÓN:");
+console.log((120-18)/6);
